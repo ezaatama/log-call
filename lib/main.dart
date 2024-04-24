2bc7ab09dabc6f02ef995217684440a2fb162214
ezaatama <ezatama33@gmail.com>
2024-04-24 11:10:41 +0700

    range date log call

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,25 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Iterable<CallLogEntry> _callLogEntries = <CallLogEntry>[];
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  void _updateDateRange(DateTime dateTime) {
+    if (_startDate == null || dateTime.isBefore(_startDate!)) {
+      _startDate = dateTime;
+    }
+    if (_endDate == null || dateTime.isAfter(_endDate!)) {
+      _endDate = dateTime;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _callLogEntries = <CallLogEntry>[];
+    _startDate = null;
+    _endDate = null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +53,7 @@ class _MyAppState extends State<MyApp> {
 
       // Format tanggal, bulan, dan tahun menjadi string
       String formattedDate = '$year-$month-$day';
+      _updateDateRange(dateTime);
 
       children.add(
         Column(
@@ -58,6 +78,12 @@ class _MyAppState extends State<MyApp> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Date Range: ${_startDate != null && _endDate != null ? '${_startDate!.toString()} - ${_endDate!.toString()}' : 'No data'}',
+                  style: mono),
+            ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
